20be0ea0c282d515e584b3e61e0b2e56f4648c9e
Samuel Louviot <44468686+Sam54000@users.noreply.github.com>
2021-12-21 16:16:54 +0100

    Add files via upload

---
+++ Data_Procesing.m
@@ -0,0 +1,42 @@
+
+
+%% Data preparation
+% Import from brainstorm the SEEG file
+
+PatientName = 'LIN_NA'; %Put the patient code
+comment = 'Modulation_percentage_Z_score_P3'; %name of the file Put the results of the calculation (SBL or Z score or SNR)
+experimentName = 'Percentage values'; %Name of the folder Put raw values if you want to plot only for P1 or P2 or P3
+                               %Put percentage if you want to plot the
+                               %percentage of modulation between tDCS
+                               %states (P1/P2/P3)
+
+ %% If you need to calculate percentage
+val(:,1) = cellfun(@(x) str2double(x), tableP1(:,7)); %raw values P1
+val(:,2) = cellfun(@(x) str2double(x), tableP2(:,7)); %raw values P2
+val(:,3) = cellfun(@(x) str2double(x), tableP3(:,7)); %raw values P3
+P1(:,7) = num2cell(val(:,1));
+P2(:,7) = num2cell(val(:,2));
+P3(:,7) = num2cell(val(:,3));
+
+percentage(:,1) = (val(:,2)-val(:,1)).*100./val(:,1); %P2 VS P1
+percentage(:,2) = (val(:,3)-val(:,1)).*100./val(:,1); %P3 VS P1
+percentage(:,3) = (val(:,3)-val(:,2)).*100./val(:,1); %P2 VS P3
+
+perc1 = tableP1;
+perc2 = tableP1;
+perc3 = tableP1;
+
+perc1(:,7) = num2cell(percentage(:,1));
+perc2(:,7) = num2cell(percentage(:,1));
+perc3(:,7) = num2cell(percentage(:,1));
+
+
+%% Export to matlab
+% You will have to run this function for each tDCS state you have (P1,P2
+% and P3) or for each percentage comparison you have (P1 vs P2, P1 vs P3
+% and P2 vs P3)
+
+letswave_table = perc3; % if you want to export to brainstorm the raw value of Pn put Pn (n = 1;2 or 3) here, or if
+% you want to export to brainstorm the percentage, replace tableP1 by percN
+% (with N = 1;2 or 3)
+values = convert_letswave_to_brainstorm(bst_seeg,PatientName,experimentName,letswave_table,comment);
